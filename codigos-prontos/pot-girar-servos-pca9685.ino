0820acf55c1f0c092a1f4b7d20f741eed4ef7aeb
Cauan Carlos <124281600+cauancarlosrd@users.noreply.github.com>
2024-05-16 13:10:49 -0300

    Update pot-girar-servos-pca9685.ino

--- codigos-prontos/pot-girar-servos-pca9685.ino
+++ codigos-prontos/pot-girar-servos-pca9685.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define MIN_PULSE_WIDTH        650
+#define MIN_PULSE_WIDTH        700
 #define MAX_PULSE_WIDTH       2350
 #define FREQUENCY               60
 #define pot                     A3
@@ -25,7 +25,7 @@ void moveMotor(int controlIn, int motorOut)
   
   potVal = analogRead(controlIn);                                                   //Read value of Potentiometer
   
-  pulse_wide = map(potVal, 800, 240, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  pulse_wide = map(potVal, 300, 850, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
   pulse_width = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);                //Map Potentiometer position to Motor
   
   pwm.setPWM(motorOut, 0, pulse_width);
